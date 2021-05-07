1fd4faac5c2e3e108de40881b36eab918a4020eb
Inushin <aitorsobera@gmail.com>
2021-05-07 13:53:34 +0200

    JS menu

---
+++ web/js/index2.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("a li").hover(function () {
+        $(this).css("background-color", "#2B75AE");
+    }, function () {
+        $(this).css("background-color", "#364278");
+    });
+
+});
