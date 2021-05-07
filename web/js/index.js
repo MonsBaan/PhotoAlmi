6ff9a84003dd0d0cc22f90c211a16bd6c42270ff
JonMa489 <jondimanzanos@gmail.com>
2021-05-07 13:48:46 +0200

    HTML LOGIN

--- web/js/index.js
+++ web/js/index.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
     console.log("Documento Listo");
+    $("#showPassword").click(function(){
+        $("#password").val("hola");
+    });
 
+
+
+
+
+    /*$("#showPassword").onclick(function () {
+        var cambiar = document.getElementById("password");
+        if (cambiar.type == "password") {
+            cambiar.type = "text";
+        } else {
+            cambiar.type = "password";
+        }
+    });*/
 });
