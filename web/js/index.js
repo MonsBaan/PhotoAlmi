8a993fb874e0eea58eb5477a299f6299f0945b61
JonMa489 <Jondimanzanos@gmail.com>
2021-05-11 13:40:10 +0200

    Subido ajax

--- web/js/index.js
+++ web/js/index.js
@@ -1,50 +1,36 @@
 var estado = 1;
-$(document).ready(function() {
-    console.log("Documento Listo");
-    $('#eye').click(function() {
+$(document).ready(function () {
+  console.log("Documento Listo");
+  $("#eye").click(function () {
+    if ($(this).hasClass("fa-eye-slash")) {
+      $(this).removeClass("fa-eye-slash");
 
-        if ($(this).hasClass('fa-eye-slash')) {
+      $(this).addClass("fa-eye");
 
-            $(this).removeClass('fa-eye-slash');
+      $("#password").attr("type", "text");
+    } else {
+      $(this).removeClass("fa-eye");
 
-            $(this).addClass('fa-eye');
+      $(this).addClass("fa-eye-slash");
 
-            $('#password').attr('type', 'text');
+      $("#password").attr("type", "password");
+    }
+  });
+  $("#FormularioLogin").submit(function (event) {
+    var nombreUser = $("#user").val();
+    var passUser = $("#password").val();
 
-        } else {
-
-            $(this).removeClass('fa-eye');
-
-            $(this).addClass('fa-eye-slash');
-
-            $('#password').attr('type', 'password');
-        }
-    });
-    $("#FormularioLogin").submit(function(event) {
-        $("#FormularioLogin input");
-        //COMPROBAR QUE EL USUARIO EXISTE
-        if (estado != 0) {
-            event.preventDefault();
-            mensajeError += "Este usuario no existe";
-            $("#mensajeError > p").text(mensajeError);
-            $("#mensajeError").show();
-
-        }
-    });
-});
-
-function check_username() {
-    //USUARIO NO REPETIDO
-    var username = $('#user').val();
-    var password = $('#password').val();
-    var parametros = { 'nombre': username, 'contrasena': password, 'function': 'getUser' }
+    var parametros = {
+      "nombre": nombreUser,
+      "contrasena": passUser,
+      "function": "getUser"
+    };
+    
     $.ajax({
-        data: parametros,
-        url: 'http://192.168.6.151/web/php/ajax.php',
-        type: 'post',
-        success: function(response) {
-            console.log("HA FUNCIONAO");
-        }
+      data: parametros,
+      url: "http://192.168.50.28/PhotoAlmi/web/php/ajax.php",
+      type: "post",
+      success: function (response) {},
     });
-
-}
+  });
+});
--- web/php/datos.php
+++ web/php/datos.php
@@ -1,43 +1,31 @@
 <?php
-function conectarBBDD()
+function conectarBD()
 {
-  $mysqli = new mysqli("192.168.50.1", "phpRoot", "Almi123", "QUEEN");
+  $mysqli = new mysqli("192.168.50.1", "phpRoot", "Almi123", "KalmihootWeb");
   if ($mysqli->connect_errno) {
-    echo "Fallo en la conexión: " . $mysqli->connect_errno;
+    echo "Fallo en la conexion: " . $mysqli->connect_errno;
   }
   return $mysqli;
 }
 function getUser($username, $password)
 {
-  $mysqli = conectarBBDD();
-  $sql = "SELECT * FROM trabajadores WHERE nombre = ? AND password = ?";
+  $mysqli = conectarBD();
+  $sql = "SELECT nombre, contrasena FROM trabajadores WHERE nombre = ? AND contrasena = ?";
   $sentencia = $mysqli->prepare($sql);
-  if (!$sentencia) {
-    echo "Fallo en la preparación de la sentencia " . $mysqli->errno;
-  }
+  //if (!$sentencia) { echo "Fallo en la preparación de la sentencia " . $mysqli->errno;}
   $asignar = $sentencia->bind_param("ss", $username, $password);
-  if (!$asignar) {
-    echo "Fallo al asignar parámetros: " . $mysqli->errno;
-  }
+  //if (!$asignar) { echo "Fallo al asignar parámetros: " . $mysqli->errno;}
   $ejecucion = $sentencia->execute();
-  if (!$ejecucion) {
-    echo "Fallo en la ejecución: " . $mysqli->errno;
-  }
-  //Recorrer los resultados
-  $id = -1;
+  //if (!$ejecucion) { echo "Fallo en la ejecución: " . $mysqli->errno;}
   $nombre = "";
   $contrasena = "";
-  $puesto = "";
-  $vincular = $sentencia->bind_result($id, $nombre, $contrasena, $puesto);
-  if (!$vincular) {
-    echo "Fallo al asociar parametros: " . $mysqli->errno;
-  }
+  $vincular = $sentencia->bind_result($nombre, $contrasena);
+  //if (!$vincular) { echo "Fallo al asociar parametros: " . $mysqli->errno;}
+
   if ($sentencia->fetch()) {
     $usuario = array(
-      "id_trajador" => $id,
       "nombre" => $nombre,
-      "password" => $contrasena,
-      "puesto" => $puesto
+      "password" => $contrasena
     );
   }
   $mysqli->close();
