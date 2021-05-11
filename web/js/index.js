f0d3dd768b11bc6c8b05d50ea26e7c2918c07d95
JonMa489 <jondimanzanos@gmail.com>
2021-05-11 08:21:51 +0200

    Index.html cambiado

--- web/js/index.js
+++ web/js/index.js
@@ -1,9 +1,24 @@
 $(document).ready(function () {
     console.log("Documento Listo");
-    $("#showPassword").click(function(){
-        $("#password").val("hola");
+    $('#eye').click(function(){
+       
+        if($(this).hasClass('fa-eye-slash')){
+           
+          $(this).removeClass('fa-eye-slash');
+          
+          $(this).addClass('fa-eye');
+          
+          $('#password').attr('type','text');
+            
+        }else{
+         
+          $(this).removeClass('fa-eye');
+          
+          $(this).addClass('fa-eye-slash');  
+          
+          $('#password').attr('type','password');
+        }
     });
-
 
 
 
