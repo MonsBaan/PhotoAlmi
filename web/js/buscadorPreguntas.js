65ac1a8771263556d6de9f7db2ef0d459430cba7
Inushin <aitorsobera@gmail.com>
2021-05-11 14:15:36 +0200

    Editar noticia en proceso

--- web/js/buscadorPreguntas.js
+++ web/js/buscadorPreguntas.js
@@ -1,4 +1,39 @@
 $(document).ready(function () {
 
+    $("#filtro").on("change textInput input",function()
+  {
+    var textoFiltro = $("#filtro").val().toLowerCase();
+    var campoFiltrar = $("#categorias").val();
 
+    var resto = 0;
+    if(campoFiltrar == "FOL")
+    {
+      resto = 0;
+    } else if(campoFiltrar == "categoria2")
+    {
+      resto = 1;
+    } else if(campoFiltrar == "categoria3")
+    {
+      resto = 2;
+    }
+    
+
+
+    $("#tablaPreguntas td").each(function()
+    {
+     // if(index % 8 == resto)
+      //{
+        var texto = $(this).text().toLowerCase();
+        if(!texto.includes(textoFiltro.toLowerCase()))
+        {
+          $(this).parent('tr').hide();
+        } else {
+          $(this).parent('tr').show();
+        }
+        console.log(textoFiltro);
+     // }
+    });
+    
+    
+});
 });
---
+++ web/js/editarPreguntas.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+
+    $(".panelOculto").show();
+
+
+});
