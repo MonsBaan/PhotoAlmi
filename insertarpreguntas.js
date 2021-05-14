eba626948ff8d5344d04500e7e2f863e969bab54
JonMa489 <Jondimanzanos@gmail.com>
2021-05-14 09:36:34 +0200

    preguntas 10  EZCRIPTS

--- insertarpreguntas.js
+++ insertarpreguntas.js
@@ -5,71 +5,235 @@ var preguntas = [
         "preguntas":
             [
                 {
-                    "imagen": "gatito.png",
-                    "pregunta": "¿que son los equipos de proteccion individual?",
+                    "imagen": "https://medicaloptica.es/blog/wp-content/uploads/2018/09/Tapones_antiruido_earplug_medical_optica-2.jpg",
+                    "pregunta": "No es debido utilizar tapones para protegerse del ruido:",
+                    "respuestas": [
+                        {
+                            "respuesta": "cuando se tenga que hacer un uso discontinuo de protectores auditivos",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "en el caso de ambientes extremadamente ruidosos",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "cuando se necesite concentración",
+                            "correcta": true
+                        },
+                        {
+                            "respuesta": "cuando el/la trabajador/a necesite un protector auditivo de forma continua",
+                            "correcta": false
+                        }
+                    ],
+                    "correccion": "aa",
+                    "numAciertos": 65,
+                    "numFallos": 4,
+                },{
+                    "imagen": "https://expertoherramientas.com/wp-content/uploads/2020/06/calzado-de-seguridad.fimg_.jpg",
+                    "pregunta": "Es un equipo de protección individual:",
 
                     "respuestas": [
                         {
-                            "respuesta": "epi y blas",
+                            "respuesta": "Chancla",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "Deportivas",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "calzado de seguridad",
+                            "correcta": true
+                        }
+                    ],
+                    "correccion": "aa",
+                    "numAciertos": 1,
+                    "numFallos": 32,
+                },{
+                    "imagen": "https://manain.com/wp-content/uploads/2020/03/EPIS.png",
+                    "pregunta": "No se consideran equipos de protección individual:",
+
+                    "respuestas": [
+                        {
+                            "respuesta": "Los uniformes",
+                            "correcta": true
+                        },
+                        {
+                            "respuesta": "arnes",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "manguitos",
+                            "correcta": false
+                        }
+                    ],
+                    "correccion": "aa",
+                    "numAciertos": 45,
+                    "numFallos": 33,
+                },{
+                    "imagen": "https://www.preveex.es/wp-content/uploads/2019/07/se%C3%B1alizacion-106442935_xl.jpg",
+                    "pregunta": "Las señales de seguridad pueden ser:",
+
+                    "respuestas": [
+                        {
+                            "respuesta": "luminosas",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "acústicas",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "gestuales",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "todo lo anterior es correcto",
+                            "correcta": true
+                        }
+                    ],
+                    "correccion": "aa",
+                    "numAciertos": 22,
+                    "numFallos": 46,
+                },{
+                    "imagen": "https://cdn-01.media-brady.com/store/stes/media/catalog/product/cache/9/image/85e4522595efc69f496374d01ef2bf13/1563980769/d/m/dmeu_tc40_12_1_std.lang.all.gif",
+                    "pregunta": "Esta señal indica:",
+
+                    "respuestas": [
+                        {
+                            "respuesta": "advertencia",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "prohibición",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "obligación",
+                            "correcta": true
+                        }
+                    ],
+                    "correccion": "aa",
+                    "numAciertos": 98,
+                    "numFallos": 7,
+                },{
+                    "imagen": "https://conceptodefinicion.de/wp-content/uploads/2018/03/Evacuaci%C3%B3n.jpg",
+                    "pregunta": "La evacuación del centro de trabajo se realiza cuando:",
+
+                    "respuestas": [
+                        {
+                            "respuesta": "se ve humo en el centro de trabajo",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "hay un aviso y existe riesgo para la vida de las personas",
+                            "correcta": true
+                        },
+                        {
+                            "respuesta": "cuando los sprinklers comienzan a funcionar",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "cuando se ha llamado a los bomberos",
+                            "correcta": false
+                        }
+                    ],
+                    "correccion": "aa",
+                    "numAciertos": 22,
+                    "numFallos": 3,
+                },{
+                    "imagen": "https://gs-brandschutz.de/wp-content/uploads/2019/06/back-sprinkler-rot-2.jpg",
+                    "pregunta": "Un sprinkler es:",
+
+                    "respuestas": [
+                        {
+                            "respuesta": "calzado de seguridad",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "un rociador individual.",
+                            "correcta": true
+                        }
+                    ],
+                    "correccion": "aa",
+                    "numAciertos": 756,
+                    "numFallos": 34,
+                },{
+                    "imagen": "https://i.pinimg.com/originals/29/37/5f/29375f299a3881f2c8d5c98fde0e036f.jpg",
+                    "pregunta": "Con la señalización de seguridad se pretende:",
+
+                    "respuestas": [
+                        {
+                            "respuesta": "hacer recomendaciones de seguridad",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "lograr que el trabajador se quede inmóvil cuando vea la señal",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "informar al trabajador del riesgo",
+                            "correcta": true
+                        },
+                        {
+                            "respuesta": "ordenar el tráfico de los vehículos dentro de la empresa",
+                            "correcta": false
+                        }
+                    ],
+                    "correccion": "aa",
+                    "numAciertos": 534,
+                    "numFallos": 66,
+                },{
+                    "imagen": "http://www.adecuarisk.com/wp-content/uploads/rp3.png",
+                    "pregunta": "Los resguardos pueden ser:",
+
+                    "respuestas": [
+                        {
+                            "respuesta": "fijos, distanciadores y automáticos",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "distanciadores, móviles y envolventes",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "distanciadores, móviles y envolventes",
+                            "correcta": true
+                        },
+                        {
+                            "respuesta": "regulables, con bloqueo y manuales",
+                            "correcta": false
+                        }
+                    ],
+                    "correccion": "aa",
+                    "numAciertos": 4,
+                    "numFallos": 1,
+                },{
+                    "imagen": "https://i.ytimg.com/vi/8nSXe8KDSKo/maxresdefault.jpg",
+                    "pregunta": "Los contactos eléctricos indirectos no se evitan con:",
+
+                    "respuestas": [
+                        {
+                            "respuesta": "Diferenciales",
                             "correcta": false,
                         },
                         {
-                            "respuesta": "epis",
+                            "respuesta": "distancia",
                             "correcta": true,
                         },
                         {
-                            "respuesta": "api",
+                            "respuesta": "toma de tierra",
+                            "correcta": false
+                        },
+                        {
+                            "respuesta": "doble aislamiento",
                             "correcta": false
                         }
                     ],
-                    "correccion": "equpos de protecion individual E.P.I.S, tontito",
-                    "numAciertos": 50,
-                    "numFallos": 20,
+                    "correccion": "aa",
+                    "numAciertos": 53,
+                    "numFallos": 55,
                 },
-                {
-                    "imagen": "gatito2.jpg",
-                    "pregunta": "que es el metodo cremallera?",
-                    "categoria": "fol",
-                    "respuestas": [
-                        {
-                            "respuesta": "sistema de salida de emergencia",
-                            "correcta": true
-                        },
-                        {
-                            "respuesta": "lo que llevan las chaquetas",
-                            "correcta": false
-                        },
-                        {
-                            "respuesta": "algun grupo de rock suave",
-                            "correcta": false
-                        }
-                    ],
-                    "correccion": "es un sistema de salida organiza en caso de emergencia",
-                    "numAciertos": 50,
-                    "numFallos": 20,
-                },
-                {
-                    "imagen": "gatito3.jpg",
-                    "pregunta": "en caso de incendio?",
-                    "categoria": "fol",
-                    "respuestas": [
-                        {
-                            "respuesta": "seguir a jon que sabe lo que hace",
-                            "correcta": false
-                        },
-                        {
-                            "respuesta": "no seguir a jon que es terrorista",
-                            "correcta": false
-                        },
-                        {
-                            "respuesta": "jon terrorista",
-                            "correcta": true
-                        }
-                    ],
-                    "correccion": "es un sistema de salida organiza en caso de emergencia",
-                    "numAciertos": 50,
-                    "numFallos": 20,
-                }
             ]
     },
     {
