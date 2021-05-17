cbeee82e63dd8378ef540d3df8c84573790706d3
Inushin <aitorsobera@gmail.com>
2021-05-17 09:56:13 +0200

    Control sesión usuario
     -> Teléfono solo números
     -> Campos actualizados solo con datos

--- web/php/cerrarUser.php
+++ web/php/cerrarUser.php
@@ -3,18 +3,7 @@ session_start();
 
 session_destroy();
 
-/*
-unset($_SESSION["id"]);
-unset($_SESSION["name"]);
-unset($_SESSION["sur1"]);
-unset($_SESSION["sur2"]);
-unset($_SESSION["dni"]);
-unset($_SESSION["dir"]);
-unset($_SESSION["tlf"]);
-unset($_SESSION["pass"]);
-unset($_SESSION["img"]);
-unset($_SESSION["pos"]);
-    */
+
 header("location: ../login.php");
 
 ?>
