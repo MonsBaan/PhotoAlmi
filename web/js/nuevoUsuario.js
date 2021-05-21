bb421354c6d9d139640b92bc65673fe9f9eb7550
JonMa489 <Jondimanzanos@gmail.com>
2021-05-21 10:58:56 +0200

    select worker

--- web/js/nuevoUsuario.js
+++ web/js/nuevoUsuario.js
@@ -1,5 +1,26 @@
 $(document).ready(function () {
 console.log ("Documento funcional!")
+var urlDB = 'http://192.168.6.195:8080/kalmihootApi/puestos';
+//----------------------------------AJAX---------------------------------------
+let htmlcombo = "<option value='selected'>Everything...</option>";
+
+$.ajax({
+    url: urlDB,
+    type: 'get',
+    success: function (response) {
+        let arrTodo = response.data;
+
+        arrTodo.forEach(puesto => {
+            htmlcombo += 
+            '<option value=' +
+            puesto.id_puesto +
+            '>' +
+            puesto.descripcion +
+            '</option>';
+            $('#puesto').html(htmlcombo);
+        });
+    }
+});
 //----------------------------------EVENTOS JS----------------------------------
     //FUNCION DEL OJO
     $("#eye").click(function() {
