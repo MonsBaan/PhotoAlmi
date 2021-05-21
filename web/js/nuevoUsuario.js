a03693a7adb2c029b3f9eddecb98bcaf58dbec2e
JonMa489 <Jondimanzanos@gmail.com>
2021-05-21 09:28:22 +0200

    YO QUISE HACER UN PUSH

--- web/js/nuevoUsuario.js
+++ web/js/nuevoUsuario.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
 console.log ("Documento funcional!")
-
+//----------------------------------EVENTOS JS----------------------------------
+    //FUNCION DEL OJO
+    $("#eye").click(function() {
+        if ($(this).hasClass("fa-eye-slash")) {
+            $(this).removeClass("fa-eye-slash");
+            $(this).addClass("fa-eye");
+            $("#password").attr("type", "text");
+        } else {
+            $(this).removeClass("fa-eye");
+            $(this).addClass("fa-eye-slash");
+            $("#password").attr("type", "password");
+        }
+    });
 
 
 
