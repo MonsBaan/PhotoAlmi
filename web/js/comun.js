5e11a27649b29a308513584aa960f8067951734a
JonMa489 <Jondimanzanos@gmail.com>
2021-05-24 12:53:39 +0200

    hamburguesa automatica

--- web/js/comun.js
+++ web/js/comun.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
-    $("a li").hover(function () {
-        $(this).css("background-color", "#2B75AE");
-    }, function () {
-        $(this).css("background-color", "#364278");
-    });
+	$('a li').hover(
+		function () {
+			$(this).css('background-color', '#2B75AE');
+		},
+		function () {
+			$(this).css('background-color', '#364278');
+		}
+	);
 
+	
 });
