3474c7fe52a0cfbe009557b97e2f288bf464eb01
Inushin95 <aitorsobera@gmail.com>
2021-05-26 11:19:27 +0200

    Por favor, funciona.

--- web/php/subirImagen.php
+++ web/php/subirImagen.php
@@ -13,3 +13,22 @@ if (($_FILES["file"]["type"] == "image/pjpeg") || ($_FILES["file"]["type"] == "i
   echo "Tipo de archivo no soportado";
 }
 
+/*
+<?php
+$cont = 0;
+
+if (($_FILES["file"]["type"] == "image/pjpeg") || ($_FILES["file"]["type"] == "image/jpeg")  || ($_FILES["file"]["type"] == "image/png")  || ($_FILES["file"]["type"] == "image/gif")) {
+
+
+  if (move_uploaded_file($_FILES["file"]["tmp_name"], "../source/image/preguntas/" . $cont)) {
+    echo $cont;
+  } else {
+    echo "StaleMans.png";
+  }
+} else {
+  echo "Tipo de archivo no soportado";
+}
+
+
+*/
+
